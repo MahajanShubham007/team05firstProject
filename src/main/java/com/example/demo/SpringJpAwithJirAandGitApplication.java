db48e6d14e061e22e60cb98d8fd1d125e1f55107
MahajanShubham007 <shubhammahajan16280@gmail.com>
2022-11-11 16:20:29 +0530

    gitandjira

---
+++ src/main/java/com/example/demo/SpringJpAwithJirAandGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpAwithJirAandGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpAwithJirAandGitApplication.class, args);
+	}
+
+}
